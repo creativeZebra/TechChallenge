bcda7ac3c42dcd178d1b0b89443bfcf527ec5da5
Selina H <hochstrat-s@gmx.de>
2023-01-10 22:04:34 +0100

    building info design

--- src/components/buildingInfo/Body1.js
+++ src/components/buildingInfo/Body1.js
@@ -1,7 +1,6 @@
 import React from "react";
 import tw from "twin.macro"; //eslint-disable-line
 import { css } from "styled-components/macro"; //eslint-disable-line
-import styled from "styled-components";
 import { ReactComponent as SignUpIcon } from "feather-icons/dist/icons/plus.svg";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
@@ -10,11 +9,19 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Box from '@mui/material/Box';
+import InputAdornment from '@mui/material/InputAdornment';
+import Home from '@mui/icons-material/Home';
+import TravelExplore from '@mui/icons-material/TravelExplore';
+import HomeWork from '@mui/icons-material/HomeWork';
+import CalendarMonth from '@mui/icons-material/CalendarMonth';
 
 const FormContainer = tw.div`w-full flex-1 mt-8`;
 const Form = tw.form`mx-auto max-w-xs`;
 const MainContent = tw.div`min-h-10 mb-10 -m-2 mt-10 flex flex-col items-center`;
+
 
 function Body1() {
   const [age, setAge] = React.useState("");
@@ -38,19 +45,40 @@ function Body1() {
   return (
     <MainContent>
       <Typography variant="h4" gutterBottom>
-        Enter Building Information
+        Enter Basic Building Information
       </Typography>
+      <Typography variant="h6" gutterBottom display="block">
+        You give us the following information about your building
+        and we search for the standard values of a reference building.
+      </Typography>
+      <Box
+        component="img"
+        sx={{
+          height: 290,
+          width: 350,
+          maxHeight: { xs: 300, md: 290 },
+          maxWidth: { xs: 350, md: 320 },
+          marginLeft: 0,
+        }}
+        alt="Hero House Image"
+        src="https://www.freevector.com/uploads/vector/preview/27652/Renovation_vector_5.jpg"
+      />
       <FormContainer>
         <Form>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <Home sx={{ color: 'action.active', mr: 1, my: 2 }} />
           <TextField
             id="outlined-basic"
-            label="Address"
+            label="Building Address"
             variant="outlined"
-            sx={{ mt: 3, width: "32ch" }}
+            sx={{ mt: 0.5, width: "32ch" }}
             color="success"
           />
+          </Box>
           <FormControl fullWidth sx={{ mt: 3, width: "32ch" }}>
-            <InputLabel id="demo-simple-select-label" color="success">
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <TravelExplore sx={{ color: 'action.active', mr: 1, my: 2 }} />
+            <InputLabel id="demo-simple-select-label" color="success" sx={{ ml:4 }}>
               Climate Region
             </InputLabel>
             <Select
@@ -61,15 +89,36 @@ function Body1() {
               onChange={handleClimateChange}
               sx={{ width: "36ch" }}
               color="success"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Home />
+                  </InputAdornment>
+                ),
+              }}
             >
-              <MenuItem value={1}>1 - XY</MenuItem>
-              <MenuItem value={2}>2 - XY</MenuItem>
-              <MenuItem value={3}>3 - XY</MenuItem>
-              <MenuItem value={4}>4 - XY</MenuItem>
+              <MenuItem value={1}>Region 1 - Bremerhaven</MenuItem>
+              <MenuItem value={2}>Region 2 - Rostock</MenuItem>
+              <MenuItem value={3}>Region 3 - Hamburg</MenuItem>
+              <MenuItem value={4}>Region 4 - Potsdam</MenuItem>
+              <MenuItem value={5}>Region 5 - Essen</MenuItem>
+              <MenuItem value={6}>Region 6 - Bad Marienberg</MenuItem>
+              <MenuItem value={7}>Region 7 - Kassel</MenuItem>
+              <MenuItem value={8}>Region 8 - Braunlage</MenuItem>
+              <MenuItem value={9}>Region 9 - Chemnitz</MenuItem>
+              <MenuItem value={10}>Region 10 - Hof</MenuItem>
+              <MenuItem value={11}>Region 11 - Fichtelberg</MenuItem>
+              <MenuItem value={12}>Region 12 - Mannheim</MenuItem>
+              <MenuItem value={13}>Region 13 - Passau</MenuItem>
+              <MenuItem value={14}>Region 14 - Stötten</MenuItem>
+              <MenuItem value={15}>Region 15 - Garmisch-Partenkirchen</MenuItem>
             </Select>
+            </Box>
           </FormControl>
           <FormControl fullWidth sx={{ mt: 3, width: "32ch" }}>
-            <InputLabel id="demo-simple-select-label" color="success">
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <HomeWork sx={{ color: 'action.active', mr: 1, my: 2 }} />
+            <InputLabel id="demo-simple-select-label" color="success" sx={{ ml:4 }}>
               Building Type
             </InputLabel>
             <Select
@@ -87,9 +136,12 @@ function Body1() {
               <MenuItem value={4}>Apartment Block</MenuItem>
               <MenuItem value={5}>Company Building</MenuItem>
             </Select>
+            </Box>
           </FormControl>
           <FormControl fullWidth sx={{ mt: 3, width: "32ch" }}>
-            <InputLabel id="demo-simple-select-label" color="success">
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <CalendarMonth sx={{ color: 'action.active', mr: 1, my: 2 }} />
+            <InputLabel id="demo-simple-select-label" color="success" sx={{ ml:4 }}>
               Construction Year Class
             </InputLabel>
             <Select
@@ -114,11 +166,12 @@ function Body1() {
               <MenuItem value={110}>2010 ... 2015</MenuItem>
               <MenuItem value={120}>2016 ...</MenuItem>
             </Select>
+            </Box>
           </FormControl>
           <Button
-            sx={{ ml: 3.5, mt: 3, width: "34ch" }}
+            sx={{ ml: 0.5, mt: 3, width: "41ch", mb: 3 }}
             color="success"
-            component={Link} 
+            component={Link}
             to="/calculation"
             variant="contained"
           >
